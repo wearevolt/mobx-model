7fa8270e88307278e58ace063134b042c2acb802
Alexander Ponomarev <ikidoit@gmail.com>
2016-01-07 12:55:23 +0300

    initial commit

---
+++ node_modules/qs/lib/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Load modules
+
+const Stringify = require('./stringify');
+const Parse = require('./parse');
+
+
+// Declare internals
+
+const internals = {};
+
+
+exports.stringify = Stringify;
+exports.parse = Parse;
