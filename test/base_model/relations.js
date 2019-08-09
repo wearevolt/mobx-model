647b99df11c7eda725887af21f59ea02287f49cc
staltec <staltec@gmail.com>
2019-08-09 09:54:13 +0300

    Add reverse relation tests

--- test/base_model/relations.js
+++ test/base_model/relations.js
@@ -1,109 +1,111 @@
 import { expect } from 'chai';
-import { isFunction } from 'lodash';
 import { BaseModel } from '../../lib/index';
-
 
 class AlphaModel extends BaseModel {
   static attributes = {
-    value: null
+    value: null,
   };
 
   static relations = [
     {
       type: 'hasOne',
       relatedModel: 'BettaModel',
-      reverseRelation: true
+      reverseRelation: true,
     },
     {
       type: 'hasMany',
       relatedModel: 'OmegaModel',
-      reverseRelation: true
-    }
+      reverseRelation: true,
+    },
   ];
 }
 
 class BettaModel extends BaseModel {
   static attributes = {
-    name: null
+    name: null,
   };
 
   static relations = [
     {
       type: 'hasOne',
       relatedModel: 'AlphaModel',
-      reverseRelation: true
-    }
+      reverseRelation: true,
+    },
   ];
 }
 
 class OmegaModel extends BaseModel {
   static attributes = {
-    name: null
+    name: null,
   };
 
   static relations = [
     {
       type: 'hasOne',
       relatedModel: 'AlphaModel',
-      reverseRelation: true
-    }
+      reverseRelation: true,
+    },
   ];
 }
 
 const models = { AlphaModel, BettaModel, OmegaModel };
 
-
 BaseModel.getModel = function(modelName) {
   return models[modelName];
 };
 
-const topLevelJson = {
+function getAlphaModel() {
+  const topLevelJson = {
+    alpha_model: {
+      id: 1,
+      omega_model_ids: [11, 12],
+      value: 'foo',
+      betta_model: { id: 2, name: 'bar' },
+    },
 
-  model: {
-    id: 1,
-    omega_model_ids: [
-      11,
-      12,
-      13 // not existed
+    omega_models: [
+      { id: 11, name: 'Omega bar 11' },
+      { id: 12, name: 'Omega bar 12' },
     ],
-    value: 'foo',
-    betta_model: { id: 2, name: 'bar' },
-  },
+  };
+  const modelJson = topLevelJson.alpha_model;
+  return AlphaModel.set({ modelJson, topLevelJson });
+}
 
-  omega_models: [
-    { id: 11, name: 'Omega bar 11' },
-    { id: 12, name: 'Omega bar 12' }
-  ]
+function getOmegaModel() {
+  const topLevelJson = {
+    omega_model: {
+      id: 13,
+      name: 'Omega bar 13',
+      alpha_model_id: 1,
+    },
+  };
+  const modelJson = topLevelJson.omega_model;
+  return OmegaModel.set({ modelJson, topLevelJson });
+}
 
-};
-
-const modelJson = topLevelJson.model;
-const model = AlphaModel.set({ modelJson, topLevelJson });
-
+const model = getAlphaModel();
+const modelOmega = getOmegaModel();
 
 describe('Relations', () => {
-
-  it("should set model value", function() {
+  it('should set model value', function() {
     expect(model.value).to.equal('foo');
   });
 
   describe('hasOne', () => {
-
-    it("should set `hasOne`-type related model data", function() {
+    it('should set `hasOne`-type related model data', function() {
       expect(model.bettaModel.id).to.equal(2);
       expect(model.bettaModel.name).to.equal('bar');
     });
 
-    it("should have reverse related model attribute", function() {
+    it('should have reverse related model attribute', function() {
       expect(model.bettaModel.alphaModel.id).to.equal(1);
       expect(model.bettaModel.alphaModel.value).to.equal('foo');
     });
-
   });
 
   describe('hasMany', () => {
-
-    it("should set `hasMany`-type related models data", function() {
+    it('should set `hasMany`-type related models data', function() {
       expect(!!model.omegaModels).to.equal(true);
       expect(model.omegaModels[0].id).to.equal(11);
       expect(model.omegaModels[0].name).to.equal('Omega bar 11');
@@ -111,14 +113,24 @@ describe('Relations', () => {
       expect(model.omegaModels[1].name).to.equal('Omega bar 12');
     });
 
-    it("should have reverse related model attribute", function() {
+    it('should have reverse related model attribute', function() {
       expect(model.omegaModels[0].alphaModel.id).to.equal(1);
       expect(model.omegaModels[0].alphaModel.value).to.equal('foo');
       expect(model.omegaModels[1].alphaModel.id).to.equal(1);
       expect(model.omegaModels[1].alphaModel.value).to.equal('foo');
     });
-
   });
 
+  describe('Reverse relation', () => {
+    it('should have parent AlphaModel related to OmegaModel', function() {
+      expect(!!modelOmega).to.equal(true);
 
+      expect(modelOmega.alphaModel.id).to.equal(1);
+      expect(modelOmega.alphaModel.value).to.equal('foo');
+      expect(modelOmega.alphaModel.omegaModels[2].id).to.equal(13);
+      expect(modelOmega.alphaModel.omegaModels[2].name).to.equal(
+        'Omega bar 13',
+      );
+    });
+  });
 });
